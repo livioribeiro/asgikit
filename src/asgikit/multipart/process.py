d195480b07a87cb78bc2b761b8561d1d3e366139
Livio Ribeiro <livioribeiro@outlook.com>
2022-05-08 18:53:52 -0300

    avoid dangling open file

--- src/asgikit/multipart/process.py
+++ src/asgikit/multipart/process.py
@@ -25,27 +25,31 @@ async def process_form(
     current_form_file: tuple[str, UploadedFile] = None
     current_form_file_fd = None
 
-    async for event in parse_multipart(data_stream, boundary):
-        if event.event_type != EventType.FILE_DATA and current_form_file is not None:
-            name, uploaded_file = current_form_file
-            result[name] = uploaded_file
-            current_form_file = None
+    try:
+        async for event in parse_multipart(data_stream, boundary):
+            if event.event_type != EventType.FILE_DATA and current_form_file is not None:
+                name, uploaded_file = current_form_file
+                result[name] = uploaded_file
+                current_form_file = None
+                await loop.run_in_executor(executor, os.close, current_form_file_fd)
+                current_form_file_fd = None
+
+            if event.event_type == EventType.FORM_FIELD:
+                result[event.event_value.name] = event.event_value.value
+            elif event.event_type == EventType.FORM_FILE:
+                current_form_file_fd, temp_name = tempfile.mkstemp()
+
+                current_form_file = event.event_value.name, UploadedFile(
+                    event.event_value.filename,
+                    event.event_value.content_type,
+                    temp_name,
+                )
+            elif event.event_type == EventType.FILE_DATA:
+                await loop.run_in_executor(
+                    executor, os.write, current_form_file_fd, event.event_value
+                )
+    finally:
+        if current_form_file_fd is not None:
             await loop.run_in_executor(executor, os.close, current_form_file_fd)
-            current_form_file_fd = None
-
-        if event.event_type == EventType.FORM_FIELD:
-            result[event.event_value.name] = event.event_value.value
-        elif event.event_type == EventType.FORM_FILE:
-            current_form_file_fd, temp_name = tempfile.mkstemp()
-
-            current_form_file = event.event_value.name, UploadedFile(
-                event.event_value.filename,
-                event.event_value.content_type,
-                temp_name,
-            )
-        elif event.event_type == EventType.FILE_DATA:
-            await loop.run_in_executor(
-                executor, os.write, current_form_file_fd, event.event_value
-            )
 
     return result
