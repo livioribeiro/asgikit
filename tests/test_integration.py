8f059dc0d52c5f5d5bc9128214a816a2ba68ba20
Livio Ribeiro <livioribeiro@outlook.com>
2023-12-18 08:07:15 -0300

    add integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,14 @@
+from httpx import AsyncClient
+
+from asgikit.requests import Request
+from asgikit.responses import respond_text
+
+
+async def test_request_response():
+    async def app(scope, receive, send):
+        request = Request(scope, receive, send)
+        await respond_text(request.response, "Ok")
+
+    client = AsyncClient(app=app)
+    response = await client.get("http://localhost:8000/")
+    assert response.text == "Ok"
